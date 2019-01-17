6f58007a45c0826656a2cfd8507dd2c2f4a54807
Fede Cano <fede.cano118@gmail.com>
2019-01-17 16:46:17 -0300

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,11 +14,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	// socket.emit('newMessage', {
-	// 	from: 'tyrion',
-	// 	text: 'sup?',
-	// 	createdAt: 1245
-	// });
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to chat app'
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined: pablito54'
+	});
 
 	socket.on('createMessage', (msg) => {
 		console.log('message recived: ', msg);
@@ -27,6 +31,12 @@ io.on('connection', (socket) => {
 			text: msg.text,
 			createdAt: new Date().getTime()
 		});
+
+		// socket.broadcast.emit('newMessage', {
+		// 	from: msg.from,
+		// 	text: msg.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
