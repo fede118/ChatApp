b23178eb46f6892dfc695c5ae9f38d45277d05a7
Fede Cano <fede.cano118@gmail.com>
2019-01-18 10:50:52 -0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ socket.on('newLocationMessage', function (msg) {
 	var li = $(`<li></li>`);
 	var a = $(`<a target="_blank">My current location</a>`);
 
-	li.text(`${msg.from}`);
+	li.text(`${msg.from}: `);
 	a.attr('href', msg.url);
 
 	li.append(a);
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function (msg) {
 $('#message-form').on('submit', function (e) {
 	e.preventDefault();
 
+	var messageTextbox = $('[name=message]')
+
 	socket.emit('createMessage', {
 		from: 'User',
-		text: $('[name=message]').val()
+		text: messageTextbox.val()
 	}, function () {
-
+		messageTextbox.val('');
 	});
 });
 
@@ -42,7 +44,10 @@ locationButton.on('click', function () {
 		return alert('Geolocation not supported by your browser');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending...');
+
 	navigator.geolocation.getCurrentPosition(function (position) {
+		locationButton.removeAttr('disabled').text('Send Location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
