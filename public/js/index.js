3506a4ada79488279513babe03f5e16ae27ae795
Fede Cano <fede.cano118@gmail.com>
2019-01-18 13:37:51 -0300

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var LastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + LastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+};
+
 socket.on('connect', function () {
 	console.log('Connected to server');
-
 });
 
 socket.on('disconnect', function () {
@@ -19,6 +33,7 @@ socket.on('newMessage', function (msg) {
 	});
 
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (msg) {
@@ -31,6 +46,7 @@ socket.on('newLocationMessage', function (msg) {
 	});
 
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
